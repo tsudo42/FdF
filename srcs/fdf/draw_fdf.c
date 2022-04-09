4cb3ca90f5511b2f76c38c75b415bbdd6916db9c
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-04-09 23:26:01 +0900

    Fix drawing order depending on the x-y angle

--- srcs/fdf/draw_fdf.c
+++ srcs/fdf/draw_fdf.c
@@ -12,40 +12,9 @@
 
 #include "fdf.h"
 
-static void	draw_fdf_inverse(t_fdf *fdf)
+static void	draw_fdf_iter_x(t_fdf *fdf, int x, int y, int is_angle_positive)
 {
-	int	x;
-	int	y;
-
-	y = fdf->map_height - 1;
-	while (y >= 0)
-	{
-		x = fdf->map_width - 1;
-		while (x >= 0)
-		{
-			if (x - 1 >= 0)
-				draw_line(fdf, &(fdf->img), fdf->map[y][x], fdf->map[y][x - 1]);
-			if (y - 1 >= 0)
-				draw_line(fdf, &(fdf->img), fdf->map[y][x], fdf->map[y - 1][x]);
-			x--;
-		}
-		y--;
-	}
-}
-
-void	draw_fdf(t_fdf *fdf)
-{
-	int	x;
-	int	y;
-
-	reset_screen(fdf);
-	if (fdf->camera.r_xy < -M_PI_4 || M_PI_2 + M_PI_4 < fdf->camera.r_xy)
-	{
-		draw_fdf_inverse(fdf);
-		return ;
-	}
-	y = 0;
-	while (y < fdf->map_height)
+	if (is_angle_positive)
 	{
 		x = 0;
 		while (x < fdf->map_width)
@@ -56,6 +25,44 @@ void	draw_fdf(t_fdf *fdf)
 				draw_line(fdf, &(fdf->img), fdf->map[y][x], fdf->map[y + 1][x]);
 			x++;
 		}
-		y++;
+	}
+	else
+	{
+		x = fdf->map_width - 1;
+		while (x >= 0)
+		{
+			if (x + 1 < fdf->map_width)
+				draw_line(fdf, &(fdf->img), fdf->map[y][x], fdf->map[y][x + 1]);
+			if (y + 1 < fdf->map_height)
+				draw_line(fdf, &(fdf->img), fdf->map[y][x], fdf->map[y + 1][x]);
+			x--;
+		}
+	}
+}
+
+void	draw_fdf(t_fdf *fdf)
+{
+	int	y;
+	int	is_angle_positive;
+
+	reset_screen(fdf);
+	is_angle_positive = fdf->camera.r_xy > 0;
+	if (-M_2_PI < fdf->camera.r_xy && fdf->camera.r_xy < M_2_PI)
+	{
+		y = 0;
+		while (y < fdf->map_height)
+		{
+			draw_fdf_iter_x(fdf, 0, y, is_angle_positive);
+			y++;
+		}
+	}
+	else
+	{
+		y = fdf->map_height - 1;
+		while (y >= 0)
+		{
+			draw_fdf_iter_x(fdf, 0, y, is_angle_positive);
+			y--;
+		}
 	}
 }
